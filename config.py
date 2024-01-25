bc94dd5da20bcc2e158ee26b459638f438f59100
mayank-ind <83276551+mayank-ind@users.noreply.github.com>
2024-01-25 11:02:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ PASS_DB = int(os.environ.get("PASS_DB", "721"))
 #FR_TOKEN = os.environ.get("FR_TOKEN", "f0907b55-922e-414a-8d21-47c1887bb90d")
 API_KEY = os.environ.get("API_KEY", "d9bb45ffcd4b965c515a714f2680f4a7")
 API_HASH = os.environ.get("API_HASH", "d9bb45ffcd4b965c515a714f2680f4a7")
-ADMINS = int(os.environ.get("ADMINS", "5454197638","5817712634"))
+ADMINS = int(os.environ.get("ADMINS", "5454197638,5817712634"))
 LOG = [int(chat) for chat in os.getenv("LOG", "4130337183").split(",") if chat != ""]
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------#
